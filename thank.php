c5647e63101b5f955d043bca394c5b26a87607dd
VietTT23 <62922146+VietTT23@users.noreply.github.com>
2021-01-18 16:24:44 +0700

    add thank-contact and error-contact

---
+++ thank.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.0.1">
+    <title>Thank!</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="admin/assets/dist/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="admin/css/login.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4">
+                <form class="form-login">
+
+                    <div class="alert alert-success text-center" role="alert">Gửi thành công!</div>
+                    <div class="text-center"><img class="mb-4" src="images/a.jpg" alt width="80" height="80"></div>
+
+
+                    <div class="form-group">
+                        <h3 class="text-center">Cảm ơn!</h3>
+                    </div>
+                    <div class="text-center">
+                        <a href="index.php" class="btn btn-primary">Quay về trang chủ</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
